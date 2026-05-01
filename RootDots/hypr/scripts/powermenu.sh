5c63ff4f5c4997011046e2581475143a0b33b456
Rootly :3 <frostdgs2000@gmail.com>
2026-05-01 22:07:36 +0300

    Clean up action list formatting in powermenu.sh
    Removed extra spaces and quotes from action list.

--- RootDots/hypr/scripts/powermenu.sh
+++ RootDots/hypr/scripts/powermenu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Список действий (убрали лишние пробелы и кавычки)
+
 entries=" Завершение\n󰜉 Рестарт\n󰤄 Сон\n󰈆 Выход\n󰷛 Блокировка"
 
-# Вызов меню через fuzzel
+
 selected=$(echo -e "$entries" | fuzzel --dmenu -p "Action: " --lines 5 --width 20)
 
-# Логика выполнения
+
 case "$selected" in
     *Завершение*) poweroff ;;
     *Рестарт*) reboot ;;
